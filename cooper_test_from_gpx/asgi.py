89626743e8edcf327e08b1cf41191db65eef8a6c
olin000 <psg@ki.net.pl>
2023-06-29 23:06:10 +0200

    Initial commit with Django

---
+++ cooper_test_from_gpx/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cooper_test_from_gpx project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cooper_test_from_gpx.settings')
+
+application = get_asgi_application()
